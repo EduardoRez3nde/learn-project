44939f9fd29b0b14284f32f18d14a41c36c65add
Eduardo <dudurezende426@gmail.com>
2023-10-16 08:03:21 -0400

    User, Role

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Role.java
@@ -0,0 +1,57 @@
+package com.devsuperior.dslearnbds.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_roles")
+public class Role implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+    @ManyToMany(mappedBy = "roles")
+    private final Set<User> users = new HashSet<>();
+
+    public Role() {}
+
+    public Role(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(id, role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
