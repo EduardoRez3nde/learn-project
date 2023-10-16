9cd709c1ff05306e4a9010ab5b138ed3c3405352
Eduardo <dudurezende426@gmail.com>
2023-10-16 10:12:08 -0400

    Resource

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -3,6 +3,8 @@ package com.devsuperior.dslearnbds.entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -25,6 +27,8 @@ public class Offer implements Serializable {
     @JoinColumn(name = "course_id")
     private Course course;
 
+    @OneToMany(mappedBy = "offer")
+    private final List<Resource> resources = new ArrayList<>();
     public Offer() {}
 
     public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
@@ -69,6 +73,10 @@ public class Offer implements Serializable {
 
     public Course getCourse() {
         return course;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
     }
 
     @Override
